c966803f0e5259b83cad3f7ed5a5ae4d6a87738c
Gearnode <bryan@getbirdly.com>
2015-08-26 00:01:21 +0200

    adding spec

--- spec/lib/currency_converter_spec.rb
+++ spec/lib/currency_converter_spec.rb
@@ -16,12 +16,26 @@ describe CurrencyConverter do
       expect{ described_class.convert(total_currency, from: 'EUR', to: 'QQQ' ) }.to raise_error(CurrencyConverter::CurrencyUnknown)
     end
 
-    it 'when currency_from equal to currency_to should rate return one' do
-      expect( described_class.convert(total_currency, from: 'EUR', to: 'EUR').rate ).to eq(1)
+    context 'when currency_from equal currency_to' do
+
+      it 'should rate return one' do
+        expect( described_class.convert(total_currency, from: 'EUR', to: 'EUR').rate ).to eq(1)
+      end
+
+      it 'should return total_currency' do
+        expect( described_class.convert(total_currency, from: 'EUR', to: 'EUR').take ).to eq(12)
+      end
     end
 
-    it 'when curency_from equal to currency_to should take return total_currency' do
-      expect( described_class.convert(total_currency, from: 'EUR', to: 'EUR').take ).to eq(12)
+    context 'when currency_from different currency_to' do
+
+      it 'should return rate at 2000/01/03' do
+        expect( described_class.convert(total_currency, from: 'EUR', to: 'USD', at: '2000-01-03').rate ).to eq(1.009)
+      end
+
+      it 'should return total_currency converted in USD at 2000/01/03' do
+        expect( described_class.convert(total_currency, from: 'EUR', to: 'USD', at: '2000-01-03').take ).to eq(12.107999999999999)
+      end
     end
   end
 end
