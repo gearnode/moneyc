aaeb67fc4a755640c7b51b4b6a1bcf029690818d
Gearnode <bfrimin@student.42.fr>
2015-12-31 14:32:20 +0100

    delete anti-pattern

--- lib/moneyc/fixer_http_client.rb
+++ lib/moneyc/fixer_http_client.rb
@@ -6,19 +6,17 @@ module Moneyc
     def initialize(conversion_date:, reference_currency:)
       @conversion_date = conversion_date.strftime('%Y-%m-%d')
       @reference_currency = reference_currency.to_s
+      @uri = construct_uri
     end
 
     def fetch_currency_rate
-      construct_uri
       retrieve_rate
     end
 
     private
 
-    attr_reader :uri
-
     def construct_uri
-      @uri = URI("#{ API_PROTOCOL }#{ BASE_API_URL }/#@conversion_date/?base=#@reference_currency")
+      URI("#{ API_PROTOCOL }#{ BASE_API_URL }/#@conversion_date/?base=#@reference_currency")
     end
 
     def retrieve_rate
@@ -28,7 +26,7 @@ module Moneyc
     end
 
     def call_api
-      http_client.get(uri)
+      http_client.get(@uri)
     end
 
     def http_client
