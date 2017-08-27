c23f93914841ac15bfdc1525e4e9d16a9b4918af
Gearnode <friminb@gmail.com>
2017-08-27 15:57:08 +0200

    Fix API regression

--- lib/moneyc/fixer_http_client.rb
+++ lib/moneyc/fixer_http_client.rb
@@ -1,36 +1,27 @@
 module Moneyc
+  # This module deal with the fixer.io API using Net::HTTP ruby module.
   class FixerHTTPClient
-    API_PROTOCOL = 'https://'.freeze
-    BASE_API_URL = 'api.fixer.io'.freeze
-
     def initialize(conversion_date:, reference_currency:)
       @conversion_date = conversion_date.strftime('%Y-%m-%d')
       @reference_currency = reference_currency.to_s
-      @uri = construct_uri
     end
 
     def fetch_currency_rate
-      retrieve_rate
+      JSON.parse(response, symbolize_names: true).fetch(:rates)
+    rescue KeyError
+      raise(ArgumentError, 'target currency is not available')
     end
 
     private
 
-    def construct_uri
-      URI("#{ API_PROTOCOL }#{ BASE_API_URL }/#@conversion_date/?base=#@reference_currency")
-    end
-
-    def retrieve_rate
-      JSON.parse(call_api, { symbolize_names: true }).fetch(:rates)
-    rescue KeyError
-      fail ArgumentError, 'target currency is not available'
-    end
-
-    def call_api
-      http_client.get(@uri)
-    end
-
-    def http_client
-      Net::HTTP
+    def response
+      Net::HTTP.get(
+        URI::HTTPS.build(
+          host: 'api.fixer.io',
+          path: "/#{@conversion_date}",
+          query: "base=#{@reference_currency}"
+        )
+      )
     end
   end
 end
